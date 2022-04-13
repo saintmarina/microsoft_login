bbc4c3dd65a5f5ac179d722a1650d26127c742ac
Anna Maniuk <anna@maniuk.nyc>
2022-04-13 00:46:57 -0400

    register layout in progress

---
+++ app/src/main/java/com/saintmarina/microsoft_sign_in/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.saintmarina.microsoft_sign_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
