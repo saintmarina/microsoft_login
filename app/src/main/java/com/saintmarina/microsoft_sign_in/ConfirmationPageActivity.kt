6dac2161831da536b088ad88ded4d800ed8c448c
Anna Maniuk <anna@maniuk.nyc>
2022-04-13 23:48:30 -0400

    After reviewing the code, added a few TODOs and nits

--- app/src/main/java/com/saintmarina/microsoft_sign_in/ConfirmationPageActivity.kt
+++ app/src/main/java/com/saintmarina/microsoft_sign_in/ConfirmationPageActivity.kt
@@ -11,15 +11,13 @@ class ConfirmationPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_confirmation_page)
         val confirmationText = findViewById<TextView>(R.id.confirmation_text)
         val textGreeting = findViewById<TextView>(R.id.greeting_text)
+
         val bundle = intent.extras!!
-        val name = formatName(bundle.getString("name")!!)
+        val name = bundle.getString("name")!!
+        val email = bundle.getString("email")!!
+        val website = bundle.getString("website")!!
 
         textGreeting.text = getString(R.string.greeting_text, if (name.isEmpty()) "" else ", $name")
-        confirmationText.text = getString(R.string.confirmation_text, bundle.getString("email"), name, bundle.getString("website"))
-    }
-
-    private fun formatName(name: String):String {
-        if (name.isEmpty() || name.isBlank()) return name
-        return name.trim().split(" ")[0].replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        confirmationText.text = getString(R.string.confirmation_text, email, name, website)
     }
 }
--- app/src/main/java/com/saintmarina/microsoft_sign_in/RegisterActivity.kt
+++ app/src/main/java/com/saintmarina/microsoft_sign_in/RegisterActivity.kt
@@ -3,8 +3,21 @@ package com.saintmarina.microsoft_sign_in
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.saintmarina.microsoft_sign_in.databinding.ActivityRegisterBinding
+
+
+// TODO add a cross to all the editTexts fields, so user could delete everything they typed
+// TODO make the name to be a required filter
+// TODO add a * to the name, email, password hints
+// TODO add title "Create Profile" to the RegisterActivity page
+// TODO add opacity to the facebook and google" icons
+// TODO on the confirmation page: Name: Anna\n email:anna@maniuk.nyc \nwebsite:github.com/saintmarina
+// TODO the check_mark is too big. Make it smaller
+// TODO make the checkmark green
+// TODO confirmation page take out the "Hello, user!" line. Add it to the confirmation text.
+// TODO underline "Already have an account?"
+// TODO instead of "use other methods" --> Sign in with
+// TODO "Submit" -->"Register"
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -14,6 +27,7 @@ class RegisterActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
         // Validating email. Email must include @ and .
         binding.editTextEmail.setOnFocusChangeListener { _, hasFocus ->
              if (!hasFocus) {
@@ -21,23 +35,24 @@ class RegisterActivity : AppCompatActivity() {
              }
         }
 
-        //Validating password. Password must be minimum 4 letters
+        // Validating password. Password must be minimum 4 letters
         binding.editTextPassword.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
                 binding.editTextPasswordError.text = passwordValidationErrorMessage()
             }
         }
 
-        //Validating website string is not necessary, because people might provide
-        // instagram, github, linkedin, url etc.
+        // As tempting it might be to validate the website string as a URL, it might
+        // not be good: many people might want to submit something like "instagram: @username"
+        // instead of a traditional URL. We let them do it.
 
         binding.cirRegisterButton.setOnClickListener {
             if (areAllFormFieldsValid()) {
                 // Submit and start new activity
                 val intent = Intent(this, ConfirmationPageActivity::class.java).apply {
-                    putExtra("name", binding.editTextName.text.toString())
-                    putExtra("email", binding.editTextEmail.text.toString())
-                    putExtra("website", binding.editTextWebsite.text.toString())
+                    putExtra("name", binding.editTextName.text.trim())
+                    putExtra("email", binding.editTextEmail.text.trim())
+                    putExtra("website", binding.editTextWebsite.text.trim())
                 }
                 startActivity(intent)
             } else {
@@ -48,33 +63,31 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun areAllFormFieldsValid():Boolean {
-        return emailValidationErrorMessage().isEmpty() &&
-                passwordValidationErrorMessage().isEmpty()
+        return emailValidationErrorMessage() == null &&
+                passwordValidationErrorMessage() == null
     }
 
-    private fun emailValidationErrorMessage():String {
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
-
-        return if (binding.editTextEmail.text.toString().isEmpty()) {
-            "*This is required field. Enter your email address."
+    private fun emailValidationErrorMessage():String? {
+        val emailPattern = ".+@.+\\..+".toRegex()
+        // TODO make a when
+        // create a variable and trim it before doing any checks
+        return if (binding.editTextEmail.text.isEmpty()) {
+            "Please enter an email address"
         } else {
-            if (binding.editTextEmail.text.toString().trim().matches(emailPattern)) {
-                ""
+            if (binding.editTextEmail.text.trim().matches(emailPattern)) {
+                null
             } else {
-                "*This is required field. Enter a valid email address."
+                "Please enter a valid email address"
             }
         }
-
     }
 
-    private fun passwordValidationErrorMessage():String {
-        val password = binding.editTextPassword.text.toString()
+    private fun passwordValidationErrorMessage():String? {
+        val password = binding.editTextPassword.text
         return when {
-            password.isEmpty() -> "*This is required field. Enter a password"
-            password.length < 4 -> "*Password has to be minimum 4 characters"
-            else -> ""
+            password.isEmpty() -> "Please enter a password"
+            password.length < 4 -> "Password has to be minimum 4 characters"
+            else -> null
         }
-
     }
-
 }
